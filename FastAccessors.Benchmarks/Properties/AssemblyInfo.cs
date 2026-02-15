de7e4f5bbcbc2b481637ef079a8b78e57ba6fd7b
Dmitry.Garavsky <dmitry.garavsky@devexpress.com>
2026-02-15 21:40:05 +0100

    v1.0.0.2
    Changes
    - support for .NET8
    - multitarget build + GH workflow

--- FastAccessors.Benchmarks/Properties/AssemblyInfo.cs
+++ FastAccessors.Benchmarks/Properties/AssemblyInfo.cs
@@ -1,35 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("FastAccessors.Benchmarks")]
 [assembly: AssemblyDescription("Fast Accessors Library for .Net")]
 [assembly: AssemblyCompany("Dmitry Garavsky(https://github.com/DmitryGaravsky)")]
 [assembly: AssemblyProduct("FastAccessors.Benchmarks")]
-[assembly: AssemblyCopyright("Dmitry Garavsky, ©2021")]
+[assembly: AssemblyCopyright("Dmitry Garavsky, ©2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("CA7E802F-9B68-40C5-92CC-AC6D71F83176")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
